ce07b85d90ea15a2c8b81d0caaacf0463b5f311f
Elaine Trombley <elaine.trombley3@gmail.com>
2015-04-06 13:20:21 -0400

    added .gitignore & bower.json

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'ejs');
+
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -20,9 +23,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', routes);
+
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
